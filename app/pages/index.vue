637db785f953dd55ac3765f8fbfa0c747d5636bd
alephpi <maosicheng98@gmail.com>
2025-10-07 00:22:20 +0200

    init

---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <UPageHero
+      title="Texo"
+      description="In-browser LaTeX OCR"
+      :links="[{
+        label: 'Snip and Recognize',
+        to: './ocr',
+        target: '_blank',
+        trailingIcon: 'i-lucide-arrow-right',
+        size: 'xl'
+      }, {
+        label: 'Star me',
+        to: 'https://github.com/alephpi/Texo',
+        target: '_blank',
+        icon: 'i-simple-icons-github',
+        size: 'xl',
+        color: 'neutral',
+        variant: 'subtle'
+      }]"
+    />
+  </div>
+</template>
