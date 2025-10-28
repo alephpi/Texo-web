a745c3cb29fab40226958ff62b5323f08c599b70
alephpi <maosicheng98@gmail.com>
2025-10-28 12:52:49 +0100

    fix: disabled button ui

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,6 +3,14 @@ export default defineAppConfig({
     colors: {
       primary: 'green',
       neutral: 'slate'
+    },
+    button: {
+      slots: {
+        base: [
+          'rounded-md font-medium inline-flex items-center disabled:cursor-not-allowed aria-disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:opacity-75 disabled:!bg-gray-400 aria-disabled:!bg-gray-400',
+          'transition-colors'
+        ]
+      }
     }
   }
 })
